15d7e7ec61e0593421acf0155946ab36281b044a
TaimoorSaeed <taimoor_saeed@hotmail.com>
2017-01-31 11:11:44 +0500

    initial-commit

---
+++ Homepwner Application (UITableView)/ItemsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Homepwner Application (UITableView)
+//
+//  Created by Nano Degree on 31/01/2017.
+//  Copyright © 2017 Swift Pakistan. All rights reserved.
+//
+
+import Foundation
---
+++ Homepwner Application (UITableView)/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Homepwner Application (UITableView)
+//
+//  Created by Nano Degree on 31/01/2017.
+//  Copyright © 2017 Swift Pakistan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
